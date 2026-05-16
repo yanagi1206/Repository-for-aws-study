8ddfb330975dfe667d17ee84bab7f16997d7b5ff
yanagi <f3521.breakindrums.yanagi@gmail.com>
2026-05-16 15:54:04 +0900

    playbookを作成

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -24,6 +24,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_company" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_all" {
+  security_group_id = aws_security_group.ec2_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
 resource "aws_vpc_security_group_ingress_rule" "allow_alb" {
   security_group_id            = aws_security_group.ec2_security_group.id
   referenced_security_group_id = aws_security_group.alb_security_group.id
